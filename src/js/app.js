0c752f79a723bd5f24a69dbf832bd49a5bc7e96e
petrborr <petrborr@gmail.com>
2022-12-21 12:56:34 +0200

    import ramda in a different way

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import "../scss/app.scss";
-import pluck from "ramda/src/pluck";
+import * as R from "ramda"
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", () => {
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
-  let classes = pluck("class", arrayToPluck);
+  let classes = R.pluck("class", arrayToPluck);
 
   const articles = document.querySelectorAll("article");
   for (let i = 0; i < 3; i++) {
